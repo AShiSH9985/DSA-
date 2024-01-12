91549ec807578d21a5daef7cea8b0e1f67d904cf
AShiSH9985 <125582224+AShiSH9985@users.noreply.github.com>
2024-01-12 09:18:32 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class array {
+
+    public static void main(String[] args){
+        Scanner a=new Scanner(System.in);
+        int n=a.nextInt();
+        int b [] = new int [n];
+        for(int i=0;i<n;i++){
+            b[i]=a.nextInt();
+        }
+       
+    }
+}
